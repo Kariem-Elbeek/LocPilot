0586c6347071b52ca403c26d7480a7c76c9dc0cb
Kariem88 <abdelkariem.asaad@gmail.com>
2024-01-15 11:17:13 +0200

    photo edited

--- js/main.js
+++ js/main.js
@@ -5,17 +5,31 @@ let navbarHeight = $('#navbar').height();
 let homeHeight = $('#home').height();
 let windowWidth = $(window).width();
 let windowHeight = $(window).height();
-let ueberUnsTop = $('#ueber-uns').offset().top;
-let ueberHeadingHeight = $('#ueber-uns .section-heading').height();
-let ueberUnsHeight = $('#ueber-uns').height();
-let dienstTop = $('#dienstleistungen').offset().top;
-let dienstHeadingHeight = $('#dienstleistungen .section-heading').height();
-let referenzenTop = $('#referenzen').offset().top;
-let refnzHeadingHeight = $('#referenzen .section-heading').height();
-let ueberUnsContentTop = $('#ueber-uns .content').offset().top;
-let dienstContentTop = $('#dienstleistungen .content').offset().top;
-let referenzenContentTop = $('#referenzen .content').offset().top;
-let fixedFrameUpperTop = $('#fixed-frame-top, #fixed-frame-left, #fixed-frame-right').offset().top;
+let aboutUsTop = $('#about-us').length ? $('#about-us').offset().top : 0;
+let aboutHeadingHeight = $('#about-us .section-heading').height();
+let aboutUsHeight = $('#about-us').height();
+// let servicesTop = $('#services').offset().top;
+let servicesTop = $('#services').length ? $('#services').offset().top : 0;
+let servicesHeadingHeight = $('#services .section-heading').height();
+let trServicesTop = $('#tr-services').length ? $('#tr-services').offset().top : 0;
+let trServicesHeadingHeight = $('#tr-services .section-heading').height();
+let locServicesTop =  $('#loc-services').length ? $('#loc-services').offset().top : 0;
+let locServicesHeadingHeight = $('#loc-services .section-heading').height();
+let trcrServicesTop = $('#trcr-services').length ? $('#trcr-services').offset().top : 0;
+let trcrServicesHeadingHeight = $('#trcr-services .section-heading').height();
+let referencesTop = $('#references').length ? $('#references').offset().top : 0;
+let refHeadingHeight = $('#references .section-heading').height();
+let whyUsTop = $('#why-us').length ? $('#why-us').offset().top : 0;
+let whyUsHeadingHeight = $('#why-us .section-heading').height();
+let aboutUsContentTop = $('#about-us .content').length ? $('#about-us .content').offset().top : 0;
+// let servicesContentTop = $('#services .content').offset().top;
+let servicesContentTop = $('#services .content').length ? $('#services .content').offset().top : 0;
+let trServicesContentTop = $('#tr-services .content').length ? $('#tr-services .content').offset().top : 0;
+let locServicesContentTop = $('#loc-services .content').length ? $('#loc-services .content').offset().top : 0;
+let trcrServicesContentTop = $('#trcr-services .content').length ? $('#trcr-services .content').offset().top : 0;
+let referencesContentTop = $('#references .content').length ? $('#references .content').offset().top : 0;
+let whyUsContentTop = $('#why-us .content').length ? $('#why-us .content').offset().top : 0;
+let fixedFrameUpperTop = $('#fixed-frame-top, #fixed-frame-left, #fixed-frame-right').length ? $('#fixed-frame-top, #fixed-frame-left, #fixed-frame-right').offset().top : 0;
 // let fixedFrameHeight = windowHeight - navbarHeight -17;
 let fixedFrameUpper = $('#fixed-frame-top, #fixed-frame-left, #fixed-frame-right');
 
@@ -23,6 +37,18 @@ let fixedFrameUpper = $('#fixed-frame-top, #fixed-frame-left, #fixed-frame-right
 // variables for navbar
 let navId;
 let goToNavIdTop;
+
+// variables for about-us
+
+let aboutUsRoundHeight = $('#about-us .round-container').height();
+let aboutUsRoundWidth = $('#about-us .round-container').width();
+// $(selector).attr(attributeName, value);
+$('.round-container').css({'height': aboutUsRoundHeight * 2, 'width': aboutUsRoundWidth});
+
+
+// variables for backgroundImage
+let bgImageValue = $('.bg-image').attr('bg-image');
+console.log(bgImageValue);
 
 // variables for form
 
@@ -45,11 +71,13 @@ submitButton.click(function(){
 
 if (windowWidth >= 768){
     // pre-animation
-    $('#ueber-uns .content-left').css({'transform': 'translateX(-120%)', 'transition': 'all 1.5s linear'});
-    $('#ueber-uns .content-right').css({'transform': 'translateX(120%)', 'transition': 'all 1.5s linear'});
-    $('#dienstleistungen .content .content-part').css({'display': 'none'});
-    $('#referenzen .content-left').css({'transform': 'translateX(-120%)', 'transition': 'all 1.5s linear'});
-    $('#referenzen .content-right').css({'transform': 'translateX(120%)', 'transition': 'all 1.5s linear'});
+    $('#about-us .content-left').css({'transform': 'translateX(-120%)', 'transition': 'all 1.5s linear'});
+    $('#about-us .content-right').css({'transform': 'translateX(120%)', 'transition': 'all 1.5s linear'});
+    $('#services .content .content-part, #tr-services .content .content-part, #loc-services .content .content-part, #trcr-services .content .content-part').css({'display': 'none'});
+    $('#references .content-left').css({'transform': 'translateX(-120%)', 'transition': 'all 1.5s linear'});
+    $('#references .content-right').css({'transform': 'translateX(120%)', 'transition': 'all 1.5s linear'});
+    $('#why-us .content-left').css({'transform': 'translateX(-120%)', 'transition': 'all 1.5s linear'});
+    $('#why-us .content-right').css({'transform': 'translateX(120%)', 'transition': 'all 1.5s linear'});
 }
 else{
     $('.content-part').css({'display': 'none'});
@@ -70,25 +98,49 @@ $(window).scroll(function(){
     }
     
     if (windowWidth >= 768){
-        if (windowTop > ueberUnsTop - (ueberUnsHeight * .7)){
-            ueberUnsShowBig();
+        if (windowTop > aboutUsTop - (aboutUsHeight * .7)){
+            aboutUsShowBig();
         }
-        if (windowBottom > dienstTop + (dienstHeadingHeight * 1.5)){
-            dienstShowBig();
+        if (windowBottom > servicesTop + (servicesHeadingHeight * 1.5)){
+            servicesShowBig('services');
         }
-        if (windowBottom > referenzenTop + (refnzHeadingHeight * 1.5)){
-            refnzShowBig();
+        if (windowBottom > trServicesTop + (trServicesHeadingHeight * 1.5)){
+            servicesShowBig('tr-services');
+        }
+        if (windowBottom > locServicesTop + (locServicesHeadingHeight * 1.5)){
+            servicesShowBig('loc-services');
+        }
+        if (windowBottom > trcrServicesTop + (trcrServicesHeadingHeight * 1.5)){
+            servicesShowBig('trcr-services');
+        }
+        if (windowBottom > referencesTop + (refHeadingHeight * 1.5)){
+            refShowBig();
+        }
+        if (windowBottom > whyUsTop + (whyUsHeadingHeight * 1.5)){
+            whyUsShowBig();
         }
     }
     else{
-        if (windowBottom > ueberUnsContentTop + ueberHeadingHeight){
-            ueberUnsShowSmall();
+        if (windowBottom > aboutUsContentTop + aboutHeadingHeight){
+            aboutUsShowSmall();
         }
-        if (windowBottom > dienstContentTop + dienstHeadingHeight){
-            dienstShowSmall();
+        if (windowBottom > servicesContentTop + servicesHeadingHeight){
+            servicesShowSmall();
         }
-        if (windowBottom > referenzenContentTop + refnzHeadingHeight){
-            refnzShowSmall();
+        if (windowBottom > trServicesContentTop + trServicesHeadingHeight){
+            servicesShowSmall();
+        }
+        if (windowBottom > locServicesContentTop + locServicesHeadingHeight){
+            servicesShowSmall();
+        }
+        if (windowBottom > trcrServicesContentTop + trcrServicesHeadingHeight){
+            servicesShowSmall();
+        }
+        if (windowBottom > referencesContentTop + refHeadingHeight){
+            refShowSmall();
+        }
+        if (windowBottom > whyUsContentTop + whyUsHeadingHeight){
+            whyUsShowSmall();
         }
     }
 });
@@ -129,43 +181,97 @@ function showEachSlideDown(element, i)
         ); // Delay each animation based on the index
     }
 
-// section ueber-uns
+// section about-us
 
-function ueberUnsShowBig () {
-    $('#ueber-uns .content-left').css('transform', 'translateX(0%)');
-    $('#ueber-uns .content-right').css('transform', 'translateX(0%)');
+function aboutUsShowBig () {
+    $('#about-us .content-left').css('transform', 'translateX(0%)');
+    $('#about-us .content-right').css('transform', 'translateX(0%)');
 }
-function ueberUnsShowSmall () {
-    $('#ueber-uns .content .content-part').each(function(i){
+function aboutUsShowSmall () {
+    $('#about-us .content .content-part').each(function(i){
         let element = $(this);
         showEachSlideDown(element, i);
     });
 }
 
 
-// section dienstleistungen
+// section services
 
-function dienstShowBig () 
+function servicesShowBig (c) 
 {
-    $('#dienstleistungen .content .content-part').slideDown(2000);
+    $(`#${c} .content .content-part`).slideDown(2000);
 }
 
-function dienstShowSmall (){
-    $('#dienstleistungen .content .content-part').each(function(i){
+function servicesShowSmall (){
+    $('#services .content .content-part').each(function(i){
         let element = $(this);
         showEachSlideDown(element, i);
     });    
 }
+// // section services
 
-// section referenzen
+// function servicesShowBig (c) 
+// {
+//     $('#services .content .content-part').slideDown(2000);
+// }
 
-function refnzShowBig () {
-    $('#referenzen .content-left').css('transform', 'translateX(0%)');
-    $('#referenzen .content-right').css('transform', 'translateX(0%)');    
+// function servicesShowSmall (){
+//     $('#services .content .content-part').each(function(i){
+//         let element = $(this);
+//         showEachSlideDown(element, i);
+//     });    
+// }
+// // section services
+
+// function servicesShowBig () 
+// {
+//     $('#services .content .content-part').slideDown(2000);
+// }
+
+// function servicesShowSmall (){
+//     $('#services .content .content-part').each(function(i){
+//         let element = $(this);
+//         showEachSlideDown(element, i);
+//     });    
+// }
+// // section services
+
+// function servicesShowBig () 
+// {
+//     $('#services .content .content-part').slideDown(2000);
+// }
+
+// function servicesShowSmall (){
+//     $('#services .content .content-part').each(function(i){
+//         let element = $(this);
+//         showEachSlideDown(element, i);
+//     });    
+// }
+
+// section references
+
+function refShowBig () {
+    $('#references .content-left').css('transform', 'translateX(0%)');
+    $('#references .content-right').css('transform', 'translateX(0%)');    
 }
         
-function refnzShowSmall () {
-    $('#referenzen .content-part').each(function(i){
+function refShowSmall () {
+    $('#references .content-part').each(function(i){
+        let element = $(this);
+        showEachSlideDown(element, i);
+    });
+}
+
+
+// section why-us
+
+function whyUsShowBig () {
+    $('#why-us .content-left').css('transform', 'translateX(0%)');
+    $('#why-us .content-right').css('transform', 'translateX(0%)');    
+}
+        
+function whyUsShowSmall () {
+    $('#why-us .content-part').each(function(i){
         let element = $(this);
         showEachSlideDown(element, i);
     });
